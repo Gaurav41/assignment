38bea837a1a014934c15909676231caa42613d93
Gaurav Pingale <gauravpingale4@gmail.com>
2021-01-21 13:41:07 +0530

    Divide feature added

--- Functions.java
+++ Functions.java
@@ -13,7 +13,10 @@ class Functions {
 		c=(c<0)?(-c):(c); 
 		System.out.println(c);
 	}
-
+	public void divide (int a, int b) {
+		int c=a/b;
+		System.out.println(c);
+	}
 
 	public static void main(String args) {
 
